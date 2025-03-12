cf57cd6c9752add620592450b6140ff051d41a84
Студент <student@L304-15.vkipo.local>
2025-03-12 09:50:39 +0300

    Привет

---
+++ 3_sem.py
@@ -0,0 +1,66 @@
+#first_numb = int(input('Enter first numb:'))
+#second_numb = int(input('Enter second numb:'))
+#third_numb = int(input('Enter third numb:'))
+
+#max = first_numb
+
+#if max < second_numb and second_numb > third_numb:
+    #max = second_numb
+#elif max < third_numb and max > second_numb:
+    #max = third_numb
+#else: 
+ 
+#print("Max numb is -", max)
+
+
+
+#first_numb = int(input('Enter first numb:'))
+#second_numb = int(input('Enter second numb:'))
+
+#if first_numb%2 == 0 and second_numb%2 == 0:
+    #print(first_numb, second_numb, " - even")
+#elif second_numb%2 == 0:
+    #print(second_numb, "- even")
+#elif first_numb%2 == 0:
+    #print(first_numb, "- even")
+#else:
+    #print("no even numbers")
+
+
+
+
+#numb = int(input("Enter ypur number: "))
+
+#for i in range(1, numb+1):
+#    if i%2 == 0:
+#        print(i, end=' '
+#arr2 = [[1,2,3,4], [5,6,7,8]]
+
+#print(arr2)
+
+'''
+import random
+
+def view_arr(arr2):
+
+    for i in arr2:
+        print(end="\n")
+        for j in range (len(i)):
+            if j==len(i)-1:
+                print(i[j], end=";")
+            else:
+                print(i[j], end=",")
+    print(end="\n")
+
+def get_random_array(size, crit):
+    arr = random.choices(range(crit), k=size)
+    return arr
+
+def create_multi_arr(row,col,crit):
+    arr2 = []
+    for i in range(row):
+        arr2.append(get_random_array(col, crit))
+    return arr2
+
+view_arr(create_multi_arr(3,3,10))
+'''
---
+++ 6_swm.py
@@ -0,0 +1,48 @@
+#a = 25
+#n = "Alice"
+#print(f"duh {a}")
+
+
+#[:1, :2]
+#S[1:2]
+#S[1:2:3]
+#foriins
+#string1 = "Mane"
+#string2 = "Slay"
+#print(string2*4, end="")
+#print (stringl[1:3])
+
+#string1 = "ATE"
+#string2 = "Gorl"
+
+#print(string1.find(string2))
+
+#string = "Tranny"
+#print(len(string))
+
+#string1 = "I could be your girlfriend for the weekend You could be my boyfriend for the night My excuse to make a bad decision Bodies gettin close under the lights"
+#string2 = "BAD"
+#string1 = string1.upper()
+#if string2 in string1:
+#   print("Есть контакт!")
+#else:
+#    print("Miss!")
+
+#def first_last(letter, st):
+#    first = st.find(letter)
+#    if first < 0:
+#        return[None, None]
+#    else:
+#        last = st.rfind(letter)
+#    return [first, last]
+
+#print(first_last("b", "abracadabra"))
+
+'''
+from collections import Counter  
+
+def top3(st):
+    counter_top3 = Counter(st.replace(' ','')).most_common(3)
+    return ','.join([f'{letter} - {cnt}' for letter, cnt in counter_top3])
+print (top3(abracadabra))
+'''
